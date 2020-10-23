ffeec503a0e86af64682b29ef83fbff7463292ac
padhma4782 <70006587+padhma4782@users.noreply.github.com>
2020-10-23 13:52:58 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,28 +1,38 @@
-const Engine= Matter.Engine;
+const Engine = Matter.Engine;
 const World= Matter.World;
-const Bodies= Matter.Bodies;
+const Bodies = Matter.Bodies;
 
-var engine,world,box;
+var engine, world;
+var ground, ball;
 
+function setup(){
+    var canvas = createCanvas(400,400);
+    engine = Engine.create();
+    world = engine.world;
 
+    var ground_options ={
+        isStatic: true
+    }
 
-function setup() {
-  createCanvas(400,400);
-  
-  engine= Engine.create();
-  world= engine.world;
-  box= Bodies.rectangle(300,200,100,200);
-  World.add(world,box);
-  console.log(box);
-  console.log(box.position.x);
-  console.log(box.position.y);
- 
-  
+    ground = Bodies.rectangle(200,390,200,20,ground_options);
+    World.add(world,ground);
+
+    var ball_options ={
+        restitution: 1.0
+    }
+
+    ball = Bodies.circle(200,100,20, ball_options);
+    World.add(world,ball);
+
+    console.log(ground);
 }
 
-function draw() {
-  background("lightblue"); 
-  Engine.update(engine); 
-  rectMode(CENTER);
-  rect(box.position.x,box.position.y,30,100);
+function draw(){
+    background(0);
+    Engine.update(engine);
+    rectMode(CENTER);
+    rect(ground.position.x,ground.position.y,400,20);
+
+    ellipseMode(RADIUS);
+    ellipse(ball.position.x, ball.position.y, 20, 20);
 }
